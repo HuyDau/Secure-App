ecc4f723dbfbf9fa6f2f60e8d7c4552855dcbd0b
Huy Dau <65066673+HuyDau@users.noreply.github.com>
2024-11-13 18:45:54 +0700

    Create Folder and Add JWT

--- API/Program.cs
+++ API/Program.cs
@@ -1,11 +1,72 @@
+using API.Data;
+using API.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
+var JWTSetting = builder.Configuration.GetSection("JWTSetting");
+
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlite("Data Source=auth.db"));
+
+builder.Services.AddIdentity<AppUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>()
+.AddDefaultTokenProviders();
+
+builder.Services.AddAuthentication(otp =>{
+    otp.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    otp.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    otp.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(otp => { 
+    otp.SaveToken = true;
+    otp.RequireHttpsMetadata = false;
+    otp.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidAudience = JWTSetting["ValidAudience"],
+        ValidIssuer = JWTSetting["ValidIssuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(JWTSetting.GetSection("securityKey").Value!))
+
+
+    };
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>{
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme {
+        Description = @"JWT Authorization Example : 'Bearer eyeleieieekeieieie",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement(){
+        {
+            new OpenApiSecurityScheme{
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "outh2",
+                Name="Bearer",
+                In = ParameterLocation.Header,
+            },
+            new List<string>()
+        }
+    });
+});
 
 var app = builder.Build();
 
@@ -17,6 +78,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseAuthentication();
 
 app.UseAuthorization();
 
